926a6b8d6e0e114ec0ce6133b8b211ebade8cffd
Daniel03 <drodriguezag@est.utn.ac.cr>
2026-08-25 02:51:23 -0600

    agregar una nueva ventana para que cree un usuario normal su usuarioo

---
+++ App/src/app/pages/usuarios/usuario-normal-page/usuario-normal-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-normal-page',
+  imports: [],
+  templateUrl: './usuario-normal-page.html',
+  styleUrl: './usuario-normal-page.css',
+})
+export class UsuarioNormalPage {}
